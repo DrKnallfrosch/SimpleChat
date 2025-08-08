4fbff072578b201bfbdc188a076ee73cb9bf641d
DrKnallfrosch <schuettetim005@gmail.com>
2025-08-08 14:17:17 +0200

    index page on /login and root redirect to /login

--- WebApp.py
+++ WebApp.py
@@ -1,13 +1,16 @@
-from bottle import Bottle, template, static_file
+from bottle import Bottle, template, static_file, redirect
 
 class WebApp:
     def __init__(self):
         self.app = Bottle()
         self.app.route('/')(self.index)
+        self.app.route('/login')(self.login)
         self.app.route('/style/style.css')(self.style)
         self.app.route('/chat_room')(self.chat_room)
 
     def index(self):
+        return redirect('/login')
+    def login(self):
         return template('template/index.html')
 
     def style(self):
