10224b4226c097ca9606cdec78c9d0a5e036e9ee
DrKnallfrosch <schuettetim005@gmail.com>
2025-08-20 23:07:02 +0200

    add registration route and error handling for 404 and 405
    Signed-off-by: DrKnallfrosch <schuettetim005@gmail.com>

--- WebApp.py
+++ WebApp.py
@@ -5,19 +5,32 @@ class WebApp:
         self.app = Bottle()
         self.app.route('/')(self.index)
         self.app.route('/login')(self.login)
-        self.app.route('/style/style.css')(self.style)
-        self.app.route('/chat_room')(self.chat_room)
+        self.app.route('/register')(self.register)
+        self.app.route('/style/<filename:path>')(self.style)
+        self.app.route('/chat_room', method='POST')(self.chat_room)
+        self.app.error(404)(self.error404)
+        self.app.error(405)(self.error405)
 
     def index(self):
         return redirect('/login')
+
     def login(self):
         return template('template/index.html')
 
-    def style(self):
-        return static_file('style/style.css', root=".")
+    def register(self):
+        return template('template/register.html')
 
     def chat_room(self):
-        return template('template/chat_room.tpl')
+        return template('template/chat_room.html')
+
+    def error404(self, error):
+        return template('template/error404.html')
+
+    def error405(self, error):
+        return template('template/error405.html')
+
+    def style(self, filename):
+        return static_file(filename, root="./style")
 
     def run(self):
         self.app.run(host='localhost', port=8080, debug=True)
